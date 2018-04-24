3f6538f0b45312fabac55cc1c1fcae1f7f7880d1
hossamFCIH <hoss_c.ronaldo@hotmail.com>
2018-04-24 19:37:52 +0200

    V2

---
+++ Space Shooter/Assets/Scripts/Projectitle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectitle : MonoBehaviour {
+
+	// Use this for initialization
+	private static float _movSpeed = 15.0f;
+	private static float _dmgAmount = 10.0f;
+	public static float MoveSpeed {
+		get{ 
+			return _movSpeed;
+		}
+		set { 
+			_movSpeed = value;
+		}
+	}
+
+	public static float dmgAmount{
+		get{ 
+			return _dmgAmount;
+		}
+		set{ 
+			_dmgAmount = value;
+		}
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.Up * _movSpeed * Time.DeltaTime);
+	}
+	private void LateUpdate(){
+	
+		if (Transform.position.y >= 10.5f) {
+			Destroy (gameObject);
+		}
+	} 
+}
